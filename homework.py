da001d9f96740cab40bd48247577825ab80fb815
Leonid2377 <leonid2377@mail.ru>
2022-02-20 13:47:33 +0300

    for review 1

--- homework.py
+++ homework.py
@@ -55,16 +55,16 @@ def get_api_answer(current_timestamp):
 
 def check_response(response):
     """Проверка ответа от сервера Яндекс Практикума."""
-    if response.get('homeworks') is None:
+    if response is None:
         logger.error('Ответ сервера не соответствует ожиданиям')
         raise ValueError('Ошибка ответа сервера')
     if type(response) != dict:
         logger.error('Ответ сервера не список')
         raise TypeError('Неверный тип данных')
-    # if not isinstance(response['homeworks'], list):
-    #     logger.error('Неверный тип данных')
-    #     raise TypeError('под ключом `homeworks`'
-    #                     ' домашки приходят не в виде списка')
+    if not isinstance(response['homeworks'], list):
+        logger.error('Неверный тип данных')
+        raise TypeError('под ключом `homeworks`'
+                        ' домашки приходят не в виде списка')
     try:
         response['homeworks']
     except Exception as error:
